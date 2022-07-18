ea1449a860c36052e1d533a0f8f87eeb390f245b
Ran Kirlian <rankirlian@gmail.com>
2022-07-18 19:46:39 +0200

    Refactor asyncFunc code

--- src/helpers/asyncFuncs.js
+++ src/helpers/asyncFuncs.js
@@ -1,23 +1,26 @@
 export const promiseFunc = (request) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-  if (request) {
-    resolve (`take your ${request} back`)
-  }else
-    reject ("Request could not be satisfied :(")
+      if (request) {
+        resolve(`take your ${request} back`)
+      } else
+        reject("Request could not be satisfied :(")
     }, 1000);
 
   })
 };
 
-export const asyncFunc = (request) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (request) {
-    resolve (`take your ${request} back`)
-  }else
-    reject ("Request could not be satisfied :(")
-    }, 1000);
+export const asyncFunc = async () => {
 
-  })
+  try {
+    const resp = await fetch('https://reqres.in/api/users?page=2')
+    const { data } = await resp.json();
+    console.log(data)
+    return data
+
+  } catch (error) {
+    console.log(error)
+    return ("data not reachable")
+  }
+
 }
