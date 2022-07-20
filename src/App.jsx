bf4f17752452b9539243718fbc44d2d61b5e3eca
Ran Kirlian <rankirlian@gmail.com>
2022-07-20 10:59:53 +0200

    Add SimpleComp component call in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { asyncFunc } from './helpers/asyncFuncs'
+import { SimpleComp } from './components/SimpleComp';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   const getDataFromApi = async () => {
     const response = await asyncFunc();
@@ -26,17 +26,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SimpleComp value="some text" />
     </div>
   )
 }
